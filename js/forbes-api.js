0552904e56077aceeb076ced98f384cf728d086f
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-03-07 15:58:41 +0600

    give up

--- js/forbes-api.js
+++ js/forbes-api.js
@@ -1,22 +1,36 @@
-const url = `https://www.forbes.com/forbesapi/person/rtb/0/-estWorthPrev/true.json`;
+// const url = `https://www.forbes.com/forbesapi/person/rtb/0/-estWorthPrev/true.json`;
 const billionaireContainer = document.getElementById('billionaire-container');
 
 
+
 function addItemToIndex(){
-  fetch(url)
-  .then(res => res.json())
-  .then(data => displayItems(data.personList.personsLists));
-  
+  fetch("https://www.forbes.com/forbesapi/person/rtb/0/-estWorthPrev/true.json")
+  .then(response => response.json())
+  .then(json => console.log(json.personList.personsLists[0].person.name))
+  .then(json => displayOnlyItem(json.personList.personsLists[0].person.name))
+  .then(json => displayItems(json.personList.personsLists))
 }
 
-function displayItems(ara) {
-  for (const i of ara) {
-    billionaireContainer.innerHTML += `
-      <li>${i.rank} => ${i.person.name}</li>
-      `
-    // console.log(`${i.rank} => ${i.person.name}`);
-  }
-}
+// function displayItems(ara) {
+//   let i = 0;
+//   for (const obj of ara) {
+//     billionaireContainer.innerHTML += `
+//       <li>${obj.id} => ${obj.title}</li>
+//       `
+//       i++;
+//       if(i===10){
+//         break;
+//       }
+//     // console.log(`${i.rank} => ${i.person.name}`);
+//   }
+// }
 
+
+function displayOnlyItem(item) {
+  billionaireContainer.innerHTML += `<li>item</li>`;
+  billionaireContainer.innerHTML += `<li>${item}</li>`;
+}
 
 addItemToIndex();
+a=5;
+displayOnlyItem(a);
