361745c10eb39c969f76079674efceda1bafad32
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-03-09 19:34:33 +0600

    little modification

--- js/forbes-api.js
+++ js/forbes-api.js
@@ -20,7 +20,8 @@ function displayItems(ara) {
     <figure><img src="${(ara[i].imageExists)? ara[i].squareImage : ('img/img-placeholder/'+ara[i].person.name[0].toLowerCase()+'.jpeg')}" alt="${ara[i].person.name}" class="max-h-[384px]"></figure>
     <div class="card-body">
       <h2 class="card-title">${ara[i].person.name}</h2>
-      <p>Rank: ${ara[i].rank+1}</p>
+      <p>Rank: #${ara[i].rank+1}</p>
+      <p>Country of Citizen Ship: <span class='font-bold'>${ara[i].countryOfCitizenship}<span></p>
         <div class="card-actions justify-end">
         <button class="btn btn-primary">$${ara[i].finalWorth} M</button>
         </div>
@@ -31,8 +32,8 @@ function displayItems(ara) {
 }
 
 function loadMore(){
-  billionaireContainer.innerHTML = '';
-  maxCart = maxCart+50;
+  // billionaireContainer.innerHTML = '';
+  maxCart = maxCart+100;
   addItemToIndex();
 }
 
