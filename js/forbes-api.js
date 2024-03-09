abbf946c93e9c0d4a98f2c823175b054b4a38b9b
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-03-09 11:42:16 +0600

    blowing

--- js/forbes-api.js
+++ js/forbes-api.js
@@ -1,27 +1,32 @@
-const url = `https://www.forbes.com/forbesapi/person/rtb/0/-estWorthPrev/true.json`;
+const url = `https://raw.githubusercontent.com/kaziMuntasirRahman/forbes-data/main/forbes-data.json`;
 const billionaireContainer = document.getElementById('billionaire-container');
 
 
 
-function addItemToIndex(){
+function addItemToIndex() {
   fetch(url)
     .then(response => response.json())
-    .then(json => console.log(json.personList.personsLists))
-  .then(json => displayOnlyItem(json.personList.personsLists[0].person.name))
-  .then(json => displayItems(json.personList.personsLists))
+    .then(json => displayItems(json.personList.personsLists))
 }
 
 function displayItems(ara) {
   let i = 0;
   for (const obj of ara) {
     billionaireContainer.innerHTML += `
-      <li>${obj.id} => ${obj.title}</li>
-      `
-      i++;
-      if(i===10){
-        break;
-      }
-    console.log(`${i.rank} => ${i.person.name}`);
+    <div class="card w-96 bg-base-100 shadow-xl">
+    <figure><img src="${obj.person.squareImage}" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">${obj.person.name}</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">$${obj.finalWorth} B</button>
+        </div>
+      </div>
+    </div>
+    `;
+    i++;
+    if(i===100)
+      break;
   }
 }
 
