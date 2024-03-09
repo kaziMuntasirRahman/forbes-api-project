68592e87f603e4af986ba427b6234ae8036a076e
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-03-09 12:23:58 +0600

    another correction

--- js/forbes-obj-from-api.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Your JavaScript code here
-  for (const n of billionaireObj.personList.personsLists) {
-    document.getElementById('billionaire-container').innerHTML += `
-      <li>${n.person.name}</li>
-    `;
-  }
-});
